78880898ca2cf87406b82e83715d2ab69e941f7b
Hemanth <hemanthhari2000@gmail.com>
2022-11-25 16:46:46 +0530

    Add. Basic UI for the Home Page with navigation transition animation setup

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,26 @@
-import '../styles/globals.css'
+import {ChakraProvider} from "@chakra-ui/react"
+import {AnimatePresence} from "framer-motion"
+import Layout from "../components/Layout";
+import theme from "../lib/theme";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps, router }) {
+
+  return (
+    <ChakraProvider theme={theme}>
+      <Layout router={router}>
+        <AnimatePresence
+          exitBeforeEnter
+          initial={true}
+          onExitComplete={() => {
+            if (typeof window !== "undefined") {
+              window.scrollTo({ top: 0 });
+            }
+          }}
+        >
+          <Component {...pageProps} key={router.route} />
+        </AnimatePresence>
+      </Layout>
+    </ChakraProvider>
+  );
 }
-
 export default MyApp
