a76af800c04e2f7c5a52ec9594a3600edbda1a19
Hemanth <hemanthhari2000@gmail.com>
2022-12-13 17:26:06 +0530

    Add. Loader for in between navigation

--- pages/_app.js
+++ pages/_app.js
@@ -1,26 +1,53 @@
-import {ChakraProvider} from "@chakra-ui/react"
-import {AnimatePresence} from "framer-motion"
+import { ChakraProvider } from "@chakra-ui/react";
+import { AnimatePresence } from "framer-motion";
+import { useEffect, useState } from "react";
 import Layout from "../components/Layout";
 import theme from "../lib/theme";
+import Router from "next/router";
+import TriangleLoader from "../components/Animations/Loading/DnaLoading";
 
 function MyApp({ Component, pageProps, router }) {
+	const [loading, setLoading] = useState(false);
 
-  return (
-    <ChakraProvider theme={theme}>
-      <Layout router={router}>
-        <AnimatePresence
-          exitBeforeEnter
-          initial={true}
-          onExitComplete={() => {
-            if (typeof window !== "undefined") {
-              window.scrollTo({ top: 0 });
-            }
-          }}
-        >
-          <Component {...pageProps} key={router.route} />
-        </AnimatePresence>
-      </Layout>
-    </ChakraProvider>
-  );
+	useEffect(() => {
+		const start = () => {
+			console.log("start");
+			setLoading(true);
+		};
+		const end = () => {
+			console.log("finished");
+			setLoading(false);
+		};
+		Router.events.on("routeChangeStart", start);
+		Router.events.on("routeChangeComplete", end);
+		Router.events.on("routeChangeError", end);
+		return () => {
+			Router.events.off("routeChangeStart", start);
+			Router.events.off("routeChangeComplete", end);
+			Router.events.off("routeChangeError", end);
+		};
+	}, []);
+
+	return (
+		<ChakraProvider theme={theme}>
+			<Layout router={router}>
+				<AnimatePresence
+					mode="wait"
+					initial={true}
+					onExitComplete={() => {
+						if (typeof window !== "undefined") {
+							window.scrollTo({ top: 0 });
+						}
+					}}
+				>
+					{loading ? (
+						<TriangleLoader />
+					) : (
+						<Component {...pageProps} key={router.route} />
+					)}
+				</AnimatePresence>
+			</Layout>
+		</ChakraProvider>
+	);
 }
-export default MyApp
+export default MyApp;
