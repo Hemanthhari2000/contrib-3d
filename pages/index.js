dc5eee577578b3a3755a687580062758e77ac8e0
Hemanth <hemanthhari2000@gmail.com>
2023-01-12 13:39:38 +0530

    Add. Responsiveness to homepage.

--- pages/index.js
+++ pages/index.js
@@ -5,15 +5,18 @@ import {
 	FormControl,
 	FormErrorMessage,
 	Input,
+	InputGroup,
+	InputLeftElement,
 	Text,
 } from "@chakra-ui/react";
-
+import Head from "next/head";
 import { useState } from "react";
 import { useRouter } from "next/router";
+
 import PageTransition from "../components/Animations/Transition/PageTransition";
 import FloatingContribImage from "../components/Animations/FloatingImage";
-import { kUltra, kUrbanist } from "../lib/constants";
-import Head from "next/head";
+import { kPrimaryFont, kSecondaryFont } from "../lib/constants";
+import { AtIcon } from "../components/AtIcon";
 
 export default function Home() {
 	const router = useRouter();
@@ -43,26 +46,35 @@ export default function Home() {
 					alignItems={"center"}
 					flexDirection={"column"}
 				>
-					<Text
-						className={kUltra.className}
-						textAlign={"center"}
-						fontSize={"55"}
-						fontWeight={"bold"}
-						letterSpacing={1.5}
-						lineHeight={1.6}
-						p={5}
-					>
-						Your Github Contribution <br /> Graph in 3D
-					</Text>
+					<Box w={"75%"}>
+						<Text
+							className={kPrimaryFont.className}
+							textAlign={"center"}
+							fontSize={{ base: "30px", md: "45px", lg: "55px" }}
+							fontWeight={"bold"}
+							letterSpacing={1.5}
+							lineHeight={1.6}
+							py={5}
+						>
+							Your Github Contribution Graph in 3D
+						</Text>
+					</Box>
 					<Box w="80%">
 						<FormControl isInvalid={isError}>
-							<Input
-								size={"lg"}
-								placeholder="Enter Github Username"
-								value={handle}
-								onChange={handleInputChange}
-								textAlign={"center"}
-							/>
+							<InputGroup size={"lg"}>
+								<InputLeftElement size={"lg"} pointerEvents="none">
+									<AtIcon />
+								</InputLeftElement>
+								<Input
+									type={"text"}
+									fontFamily={kSecondaryFont.className}
+									placeholder="github-username"
+									_placeholder={{ opacity: 1, color: "gray.500" }}
+									value={handle}
+									onChange={handleInputChange}
+								/>
+							</InputGroup>
+
 							{isError && (
 								<FormErrorMessage>
 									<Flex w={"100%"} justifyContent={"center"}>
@@ -73,7 +85,8 @@ export default function Home() {
 							<Flex w={"100%"} justifyContent={"center"}>
 								<Button
 									m={5}
-									className={kUrbanist.className}
+									className={kSecondaryFont.className}
+									fontWeight={"medium"}
 									letterSpacing={0.8}
 									colorScheme={"teal"}
 									onClick={handleOnSubmit}
