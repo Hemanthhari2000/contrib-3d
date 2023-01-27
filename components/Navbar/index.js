4c34bd2cc87a6e54903465da7aaa8a2ecfa7187c
Hemanth <hemanthhari2000@gmail.com>
2023-01-27 13:19:35 +0530

    Refactor. Navbar code

--- components/Navbar/index.js
+++ components/Navbar/index.js
@@ -1,4 +1,11 @@
-import { Box, HStack, Image, Text, useBreakpointValue } from "@chakra-ui/react";
+import {
+	Box,
+	Flex,
+	HStack,
+	Image,
+	Text,
+	useBreakpointValue,
+} from "@chakra-ui/react";
 import Link from "next/link";
 import { kSecondaryFont } from "../../lib/constants";
 import { FadeIn } from "../Animations/Transition/FadeIn";
@@ -8,48 +15,40 @@ const Navbar = ({ data }) => {
 	return (
 		<Box as="nav" position={"absolute"} top={0} left={0} w={"100%"} zIndex={10}>
 			<FadeIn>
-				{data ? (
-					<Box w={"full"}>
-						<HStack p={{ base: 5, md: 20 }}>
+				<Box w={"full"}>
+					<Flex justifyContent={"space-between"} alignItems={"center"}>
+						<HStack p={{ base: 5, md: 20 }} width={"fit-content"}>
 							<Link href={"/"}>
-								<Image
-									boxSize={imageBoxSize}
-									objectFit="cover"
-									src={"/logo.png"}
-									alt={"logo"}
-								/>
+								<HStack>
+									<Image
+										boxSize={imageBoxSize}
+										objectFit="cover"
+										src={"/logo.png"}
+										alt={"logo"}
+									/>
+									{!data ? (
+										<Text
+											className={kSecondaryFont.className}
+											fontSize={{ base: 20, md: 30 }}
+											fontWeight={"semibold"}
+											letterSpacing={1.5}
+										>
+											Contrib 3D
+										</Text>
+									) : (
+										<Text
+											className={kSecondaryFont.className}
+											fontSize={{ base: 20, md: 30 }}
+										>
+											{data}
+											{`'s`} Graph
+										</Text>
+									)}
+								</HStack>
 							</Link>
-							<Text
-								className={kSecondaryFont.className}
-								fontSize={{ base: 20, md: 30 }}
-							>
-								{data}
-								{`'s`} Graph
-							</Text>
 						</HStack>
-					</Box>
-				) : (
-					<HStack p={{ base: 5, md: 20 }} width={"fit-content"}>
-						<Link href={"/"}>
-							<HStack>
-								<Image
-									boxSize={imageBoxSize}
-									objectFit="cover"
-									src={"/logo.png"}
-									alt={"logo"}
-								/>
-								<Text
-									className={kSecondaryFont.className}
-									fontSize={{ base: 20, md: 30 }}
-									fontWeight={"semibold"}
-									letterSpacing={1.5}
-								>
-									Contrib 3D
-								</Text>
-							</HStack>
-						</Link>
-					</HStack>
-				)}
+					</Flex>
+				</Box>
 			</FadeIn>
 		</Box>
 	);
