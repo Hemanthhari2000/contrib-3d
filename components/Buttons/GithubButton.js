c722b5452a9c59456f7d717b7e90bd8a2e95b309
hemanthhari2000 <hemanthhari2000@gmail.com>
2023-03-21 22:31:55 +0530

    Add. Color mode toggle (#5)

--- components/Buttons/GithubButton.js
+++ components/Buttons/GithubButton.js
@@ -1,29 +1,30 @@
-import { Box, Link } from "@chakra-ui/react";
+import { Box, Link, useColorModeValue } from "@chakra-ui/react";
 import { GithubIcon } from "../GithubIcon";
 
 export const GithubButton = () => {
-	const borderColor = "green.600";
-	const hoverColor = "whiteAlpha.200";
-	const iconColor = "#68D391";
-	return (
-		<>
-			<Box p={{ base: 5, md: 20 }}>
-				<Link
-					href={"https://github.com/hemanthhari2000/contrib-3d"}
-					isExternal
-					title={"Source Code"}
-				>
-					<Box
-						p={2}
-						borderRadius={8}
-						border={"2px"}
-						borderColor={borderColor}
-						_hover={{ bg: hoverColor }}
-					>
-						<GithubIcon color={iconColor} />
-					</Box>
-				</Link>
-			</Box>
-		</>
-	);
+  const iconColor = "#68D391";
+  const hoverColor = useColorModeValue("green.100", "whiteAlpha.200");
+  const fillColor = useColorModeValue(iconColor, "none");
+  const borderColor = useColorModeValue(iconColor, "green.600");
+  return (
+    <>
+      <Box>
+        <Link
+          href={"https://github.com/hemanthhari2000/contrib-3d"}
+          isExternal
+          title={"Source Code"}
+        >
+          <Box
+            p={2}
+            borderRadius={8}
+            border={"2px"}
+            borderColor={borderColor}
+            _hover={{ bg: hoverColor }}
+          >
+            <GithubIcon color={iconColor} fill={fillColor} />
+          </Box>
+        </Link>
+      </Box>
+    </>
+  );
 };
