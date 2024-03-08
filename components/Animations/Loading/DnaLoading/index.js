e056a6a536a5205fc337a7dcf7add4ffd4d1cb2a
hemanthhari2000 <hemanthhari2000@gmail.com>
2024-03-08 13:43:26 +0530

    Add. Global Head title

--- components/Animations/Loading/DnaLoading/index.js
+++ components/Animations/Loading/DnaLoading/index.js
@@ -1,23 +1,19 @@
-import Head from "next/head";
 import { Triangle } from "react-loader-spinner";
 
 const TriangleLoader = () => {
-	return (
-		<>
-			<Head>
-				<title>{`Contrib 3D - Loading...`}</title>
-			</Head>
-			<Triangle
-				height="80"
-				width="80"
-				color="#4fa94d"
-				ariaLabel="triangle-loading"
-				wrapperStyle={{}}
-				wrapperClassName=""
-				visible={true}
-			/>
-		</>
-	);
+  return (
+    <>
+      <Triangle
+        height="80"
+        width="80"
+        color="#4fa94d"
+        ariaLabel="triangle-loading"
+        wrapperStyle={{}}
+        wrapperClassName=""
+        visible={true}
+      />
+    </>
+  );
 };
 
 export default TriangleLoader;
